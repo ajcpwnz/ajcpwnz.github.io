d51e62fff5d16d1413de58e6233fbaa7a427270e
ajcpwnz <ajcpwnz@d3feat.website>
2020-08-13 00:26:42 +0300

    :zap:

---
+++ precache-manifest.a1256b077a561023e5a76d790e41438f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c8c4a5c288fbeea66c66e67e5b710a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c89a5c6c97b1dd250e3",
+    "url": "/static/css/main.c300464f.chunk.css"
+  },
+  {
+    "revision": "a6efa29e2cf2fa9efe3b",
+    "url": "/static/js/2.e2a793d6.chunk.js"
+  },
+  {
+    "revision": "6e900953f82e75bb7ccced89e6806918",
+    "url": "/static/js/2.e2a793d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c89a5c6c97b1dd250e3",
+    "url": "/static/js/main.11bc1e87.chunk.js"
+  },
+  {
+    "revision": "8cf7bfbb1c66cf0f2965",
+    "url": "/static/js/runtime-main.28bd2304.js"
+  }
+]);
